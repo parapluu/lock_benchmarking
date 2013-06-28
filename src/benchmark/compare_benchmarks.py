a8b4701b76233b29dc1cb4128b6ae0003d04bba3
Kjell Winblad <kjellwinblad@gmail.com>
2013-06-28 11:47:10 +0200

    Added matplotlib output to the compare benchmark script.

--- src/benchmark/compare_benchmarks.py
+++ src/benchmark/compare_benchmarks.py
@@ -3,16 +3,19 @@
 import sys
 from os import listdir
 from os.path import join
+from os.path import dirname
 from os import mkdir
 from subprocess import Popen
 from subprocess import PIPE 
-
+from shutil import copy
 sys.argv.pop(0)
 
 if len(sys.argv) < 2:
     print """Not enough parameters:
 
-The first parameter is the output dir (where the graphs are placed).
+Optional flag -matplotlib (must be first)
+
+The first parameter is the output dir or output file if -matplotlib is specified (where the graphs are placed).
 
 The rest of the parameters are benchmark output dirs of benchmark that
 shall be compared. The benchmark results are produced by the
@@ -21,36 +24,48 @@ bin/benchmark_lock.py script.
 """
     sys.exit()
 
+use_matplotlib_out = False
+
+if sys.argv[0] == '-matplotlib':
+    use_matplotlib_out = True
+    sys.argv.pop(0)
+
 output_dir = sys.argv.pop(0)
 
 compare_dirs = sys.argv
-
-
-print 'Output dir:', output_dir
-
-print 'Compare dirs:', compare_dirs
 
 dat_files = [f for f in listdir(compare_dirs[0]) if f.endswith(".dat")]
 
 mkdir(output_dir)
 
-for file in dat_files:
-    out_file_prefix = join(output_dir, file)
-    gnuplot_pipe = Popen("gnuplot",stdin = PIPE).stdin
-    gnuplot_pipe.write("set terminal png size 1024,768\n")
-    gnuplot_pipe.write("set output \"%s.png\"\n" % out_file_prefix)
-    gnuplot_pipe.write("set title \"%s\"\n" % file)
-    gnuplot_pipe.write("set xlabel \"Number of threads\"\n")
-    gnuplot_pipe.write("set ylabel \"Microseconds/operation\"\n")
-    gnuplot_pipe.write("set yrange [0 : *]\n")
-    dir_nr = 0
-    gnuplot_pipe.write("plot ");
-    for dir in compare_dirs:
-        dir_nr = dir_nr + 1
-        gnuplot_pipe.write("\"%s\" using 1:2 title \"%s\" with linespoints" % (join(dir,file),dir))
-        if dir_nr == len(compare_dirs):
-            gnuplot_pipe.write("\n")
-        else:
-            gnuplot_pipe.write(", \\\n");
-    gnuplot_pipe.close()
-    
+if use_matplotlib_out:
+    output_file = output_dir
+    copy(join(dirname(__file__), '../src/benchmark/produce_graphs_template.py'), output_file)
+    with open(output_file, "a") as f:
+        for in_file_name in dat_files:
+            f.write('set_up_figure("%s")\n' % in_file_name)
+            for in_dir in compare_dirs:
+                f.write('plot_file("%s", "%s")\n' % (join(in_dir,in_file_name), in_dir))
+            f.write('complete_figure("%s")\n' % in_file_name)
+            f.write("\n")
+else:
+    for file in dat_files:
+        out_file_prefix = join(output_dir, file)
+        gnuplot_pipe = Popen("gnuplot",stdin = PIPE).stdin
+        gnuplot_pipe.write("set terminal png size 1024,768\n")
+        gnuplot_pipe.write("set output \"%s.png\"\n" % out_file_prefix)
+        gnuplot_pipe.write("set title \"%s\"\n" % file)
+        gnuplot_pipe.write("set xlabel \"Number of threads\"\n")
+        gnuplot_pipe.write("set ylabel \"Microseconds/operation\"\n")
+        gnuplot_pipe.write("set yrange [0 : *]\n")
+        dir_nr = 0
+        gnuplot_pipe.write("plot ");
+        for dir in compare_dirs:
+            dir_nr = dir_nr + 1
+            gnuplot_pipe.write("\"%s\" using 1:2 title \"%s\" with linespoints" % (join(dir,file),dir))
+            if dir_nr == len(compare_dirs):
+                gnuplot_pipe.write("\n")
+            else:
+                gnuplot_pipe.write(", \\\n");
+        gnuplot_pipe.close()
+        
---
+++ src/benchmark/produce_graphs_template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+
+def read_dat_file(the_file):
+    with open(the_file, 'r') as f:
+        lines = f.readlines()
+        x = []
+        y = []
+        for line in lines:
+            p = line.split()
+            x.append(float(p[0]))
+            y.append(float(p[1]))
+        return (x, y)
+
+def set_up_figure(title):
+    plt.xlabel('Number of Threads')
+    plt.ylabel('Microseconds / Operation') 
+    plt.title(title)
+
+
+def plot_file(the_file, title):
+    (x_list, y_list) = read_dat_file(the_file)
+    plt.plot(x_list, y_list, label=title)
+
+
+def complete_figure(save_file_name):
+    plt.legend()
+    plt.savefig(save_file_name + '.pdf', bbox_inches=0)
+    print save_file_name + '.pdf'
+
+
