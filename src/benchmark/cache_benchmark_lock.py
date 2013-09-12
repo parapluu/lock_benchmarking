47bb2495cda8e1fe06fa96ee1770c80d1fd6bf3b
David Klaftenegger <david.klaftenegger@it.uu.se>
2013-09-12 15:11:49 +0200

    cache measurement scripts update

--- src/benchmark/cache_benchmark_lock.py
+++ src/benchmark/cache_benchmark_lock.py
@@ -20,6 +20,8 @@ parameters = sys.argv
 
 parameters.pop(0)
 
+iterations = int(parameters.pop(0))
+
 if len(parameters) < 10:
     print """Not enough parameters:
 
@@ -62,36 +64,38 @@ iterations_rcs = parameters.pop(0).split(',')
 iterations_ncs = parameters.pop(0).split(',')
 
 
-for benchmark_id in [benchmark_name + "_" + lock_id 
-                     for benchmark_name in benchmark_names 
-                     for lock_id in lock_ids]:
-    for settings in [[pr,rts,iw,ir,incs] 
-                     for pr in percentages_reads
-                     for rts in run_times_seconds
-                     for iw in iterations_wcs
-                     for ir in iterations_rcs
-                     for incs in iterations_ncs]:
-        for pinning in pinning_settings:
-            output_file_dir_str = ('bench_results/' + 
-                                   benchmark_id + output_dir_base + '/')
-            if not os.path.exists(output_file_dir_str):
-                os.makedirs(output_file_dir_str)
-            output_file_str = (output_file_dir_str +
-                               'b_' + pinning + '_' +  '_'.join(settings) + '.dat')
-            with open(output_file_str, "w") as outfile:
-                print "\n\n\033[32m -- STARTING BENCHMARKS FOR " + output_file_str + "! -- \033[m\n\n"
-                for thread_count in thread_counts:
-                    realcmd = [bin_dir_path + '/' + benchmark_id, thread_count] + settings
-		    perfcmd = ['perf', 'stat','-B', '-e', 'r01d1:u,r02d1:u,r04d1:u,r81d0:u']
-		    command = perfcmd + realcmd
-                    if pinning=='no':
-			    (outString, outErr) = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-                    else:
-                        max_node_id = (int(thread_count)-1) / num_of_cpus_per_node
-                        nomactrl = ['numactl', '--cpunodebind=' + ",".join([str(x) for x in range(0,max_node_id+1)])]
-                        process = subprocess.Popen(nomactrl + command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-                    cachedata = subprocess.Popen(bin_dir_path + '/' + 'perf_magic', stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(outErr)[0]
-                    outfile.write(outString.rstrip('\n') + cachedata + '\n')
-                print "\n\n\033[32m -- BENCHMARKS FOR " + output_file_str + " COMPLETED! -- \033[m\n\n"
-
-
+for iteration in range(iterations):
+	print "\n\nSTARTING ITERATION " + str(iteration+1) + " / " + str(iterations) + "\n\n"
+	for (benchmark_id, lock_id) in [(benchmark_name + "_" + lock_id, lock_id)
+			     for benchmark_name in benchmark_names 
+			     for lock_id in lock_ids]:
+	    for settings in [[pr,rts,iw,ir,incs] 
+			     for pr in percentages_reads
+			     for rts in run_times_seconds
+			     for iw in iterations_wcs
+			     for ir in iterations_rcs
+			     for incs in iterations_ncs]:
+		for pinning in pinning_settings:
+		    output_file_dir_str = ('bench_results/' + 
+					   benchmark_id + '#' + output_dir_base + '#' + lock_id +  '/')
+		    if not os.path.exists(output_file_dir_str):
+			os.makedirs(output_file_dir_str)
+		    output_file_str = (output_file_dir_str +
+				       'b_' + pinning + '_' +  '_'.join(settings) + '.dat')
+		    with open(output_file_str, "a") as outfile:
+			print "\n\n\033[32m -- STARTING BENCHMARKS FOR " + output_file_str + "! -- \033[m\n\n"
+			for thread_count in thread_counts:
+			    realcmd = [bin_dir_path + '/' + benchmark_id, thread_count] + settings
+			    perfcmd = ['perf', 'stat','-B', '-e', 'r01d1:u,r02d1:u,r04d1:u,r81d0:u']
+			    command = perfcmd + realcmd
+			    if pinning=='no':
+				    (outString, outErr) = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+			    else:
+				max_node_id = (int(thread_count)-1) / num_of_cpus_per_node
+				nomactrl = ['numactl', '--cpunodebind=' + ",".join([str(x) for x in range(0,max_node_id+1)])]
+				process = subprocess.Popen(nomactrl + command, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+			    cachedata = subprocess.Popen(bin_dir_path + '/' + 'perf_magic', stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(outErr)[0]
+			    outfile.write(outString.rstrip('\n') + cachedata + '\n')
+			print "\n\n\033[32m -- BENCHMARKS FOR " + output_file_str + " COMPLETED! -- \033[m\n\n"
+	
+	print "\n\nITERATION " + str(iteration+1) + " / " + str(iterations) + " DONE!\n\n"
